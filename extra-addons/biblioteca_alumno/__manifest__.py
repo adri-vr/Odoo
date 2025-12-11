fdc8af7831eb056196322fe754173139888a1f17
Pochus <charly2adrii@gmail.com>
2025-12-11 15:46:44 +0100

    Update __manifest__.py

--- extra-addons/biblioteca_alumno/__manifest__.py
+++ extra-addons/biblioteca_alumno/__manifest__.py
@@ -1,1 +1,13 @@
-
+{
+    "name": "Biblioteca Alumno",
+    "version": "1.0",
+    "summary": "Módulo de práctica: gestión de libros",
+    "author": "Tu Nombre",
+    "category": "Teaching",
+    "depends": ["base"],
+    "data": [
+        "views/biblioteca_libro_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
